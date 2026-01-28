677f4aa6cb1b8cc7c62539bc6465eedbdc844909
kobayashi5829 <jimonjitou5884@gmail.com>
2026-01-28 11:27:07 +0900

    UIの変更、accountsの追加

---
+++ src/config/settings_develop.py
@@ -0,0 +1,9 @@
+from .settings_common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-n&z)c5$odz+k^ln4^^y%&@(d*rtouks1djqfl-7k6ym3r%ihxu'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
